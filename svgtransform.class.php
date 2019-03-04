8e736212261ac3a66d883011339db094ad248c2b
Brian Popp <bpopp@bpopp.net>
2019-03-04 14:50:55 -0600

    improved svg coverage
    The initial version didn't cover multiple coordinates in the same command and a number of other common use cases. This version isn't perfect, but it does seem to handle most SVG graphics correctly.

--- svgtransform.class.php
+++ svgtransform.class.php
@@ -12,28 +12,50 @@ class SVGTransformFix
 
     function Transform ()
     {
-        $xml = simplexml_load_file($this->in);
-        $results = $xml->xpath("//svg:g[starts-with(@transform,'scale')]");
+        $source = file_get_contents( $this->in ) ;
+
+        // this is a little lazy, but I had trouble getting simplexml_load_string to work w/ different types of ns declarations
+        $source = preg_replace  ( "/xmlns=\".*?svg\"/", "", $source );
+        $xml = simplexml_load_string( $source );
+        
+        if ( !$xml ) return $source;
+
+        
+        $results = $xml->xpath("//g[starts-with(@transform,'scale')]");
+
+        //print_r ( $results );
+
         foreach ( $results as $node )
         {
+            $this->coordTransform = null;
+            $this->strokeTransform = null;
+
             $attrs = $node->attributes();
             $transform =  (string) $attrs['transform'];
+
+
             if ( preg_match ( '|scale\((.*?)\,(.*?)\)|mis', $transform, $matches ) )
             {
                 $this->coordTransform = [ $matches[1], $matches[2] ];
+                $this->strokeTransform = $matches[1];
+            }
+            elseif ( preg_match ( '|scale\((.*?)\)|mis', $transform, $matches ) )
+            {
+                $this->coordTransform = $matches[1];
                 $this->strokeTransform = $matches[1];
             }
             // unset the @transform since we will be resetting the values manually in the next step
             $node['transform'] = '';
 
             // for each subordinate path, we will manually reset each set of coordinates
-            $paths = $node->xpath ( "descendant::svg:path" );
+            $paths = $node->xpath ( "descendant::path" );
             foreach ( $paths as $path )
             {
                 $attrs = $path->attributes();
                 $coords = (string) $attrs['d'];
                 $style = (string) $attrs['style'];
 
+                //new dbug ( $this->coordTransform );
                 $path['d'] = $this->fixcoords ( $coords );
 
                 // currently only handling the stroke-width (which seems to work)
@@ -45,8 +67,18 @@ class SVGTransformFix
                     $path['style'] = $style;
                 }
             }
+
+
         }
-        return $xml->asXML();
+
+        //exit;
+
+        $dom = dom_import_simplexml($xml)->ownerDocument;
+        $dom->formatOutput = true;
+        $dom->documentElement->setAttribute("xmlns", "http://www.w3.org/2000/svg" );
+        return $dom->saveXML();
+
+        //return $xml->asXML();
     }
 
     /***
@@ -58,24 +90,54 @@ class SVGTransformFix
 
     function fixcoords ( $coords )
     {
-        $coordlist = explode(" ", $coords);
-        $out = array();
-        foreach ( $coordlist as $coord )
+        if ( !$this->coordTransform ) return;
+
+//        $coordlist = explode(" ", $coords);
+
+        preg_match_all ( "/([a-zA-Z])([^a-zA-Z]*)/mis", $coords, $matches );
+
+
+        for ( $c = 0; $c<=sizeof($matches[0]); $c++ )
         {
-            // handle move, line, and curve commands
-            if ( in_array( $coord, ['m', 'M', 'l', 'L', 'c', 'C'] ) )
+            $command  = $matches[1][$c];
+            $value = $matches[2][$c];
+
+
+            if ( in_array ( $command, ['m', 'M', 'l', 'L', 'c', 'C', 'H', 'V', 'v', 'h'] ) )
             {
-                $out[] = $coord;
+                $out[] = $command;
+                $values = explode ( " ", trim($value) );
+
+                foreach ( $values as $value )
+                {
+                    if (  strstr ( $value, ',' ) )
+                    {
+                        list ( $x, $y ) = explode ( ",", $value );
+                        if ( is_array ( $this->coordTransform ) )
+                        {
+                            $out[] = sprintf ( '%s,%s',$x*$this->coordTransform[0],$y*$this->coordTransform[1] );
+                        }
+                        elseif ( $this->coordTransform )
+                        {
+                            $out[] = sprintf ( '%s,%s', $x*$this->coordTransform,$y*$this->coordTransform );
+                        }
+                    }
+                    else
+                    {
+                        if ( $this->coordTransform )
+                        {
+                            $out[] = sprintf ( '%s', $value*$this->coordTransform );
+                        }
+                    }
+                }
+
             }
-            // anything else should be a coordinate
-            elseif ( strstr ( $coord, ',' ) )
+            else
             {
-                list ( $x, $y ) = explode ( ",", $coord );
-                $out[] = sprintf ( '%s,%s', $x*$this->coordTransform[0],$y*$this->coordTransform[1] );
+                $out[] = $command . " " . $value;
             }
+
         }
-        return join ( ' ', $out );
+        return join ( " ", $out );
     }
-
 }
-
